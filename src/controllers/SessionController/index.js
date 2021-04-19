bbfb036c5e90cf92001951f4210049ec9710d2ec
Leonardo <leonardo.uemura>
2021-04-19 00:03:33 -0300

    session create/index/show completed

--- src/controllers/SessionController/index.js
+++ src/controllers/SessionController/index.js
@@ -2,5 +2,57 @@ const connection = require('../../database/connection')
 const crypto = require('crypto')
 
 module.exports = {
-  
+  async create(req, res) {
+    const {
+      username,
+      password
+    } = req.body
+
+    try {
+      const verifyUserAndGetId = await connection('users').where('username', username).andWhere('password', password).select('id')
+      if (verifyUserAndGetId.length !== 0) {
+        const verifyUserIdSession = await connection('session').where('user_id', verifyUserAndGetId[0].id).select('*')
+        const token = crypto.randomBytes(6).toString('HEX')
+        const timestamp = Math.floor(Date.now() / 1000)
+        const verifyUserAndGetUsername = await connection('users').where('username', username).andWhere('password', password).select('username')
+        if(verifyUserIdSession.length === 0){
+          const createUserSession = await connection('session').insert({user_id: verifyUserAndGetId[0].id, token, timestamp})
+          return res.json({message:`Nova sessão... Bem vindo ${verifyUserAndGetUsername[0].username}`,user_id: verifyUserAndGetId[0].id, token, timestamp})
+        }
+        else{
+          const updateUserSession = await connection('session').update({user_id: verifyUserAndGetId[0].id, token, timestamp}).where("user_id", verifyUserAndGetId[0].id)
+          return res.json({message:`Sessão Atualizada! Bem vindo ${verifyUserAndGetUsername[0].username}`,user_id: verifyUserAndGetId[0].id, token, timestamp})
+        }
+      }
+      else{
+        return res.json({message:"credenciais incorretas"})
+      }
+
+    }
+    catch (err) {
+      console.log('err :>> ', err);
+    }
+  },
+
+  async index(req,res){
+    try{
+      const indexSessions = await connection('session').select('*')
+      return res.json(indexSessions)
+    }
+    catch(err){
+      return res.json({message:"ocorreu um erro inesperado",...err})
+    }
+  },
+
+  async show(req,res){
+    const {id} = req.params
+    try{
+      console.log(id);
+      const showUserSession = await connection('session').select('*').where({user_id: id})
+      return res.json(showUserSession)
+    }
+    catch(err){
+      return res.json({message:"ocorreu um erro inesperado",...err})
+    }
+  }
 }
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router();
 
 const UserController = require('../controllers/UserController')
+const SessionController = require('../controllers/SessionController')
 
 router.post('/users', UserController.create)
 router.put('/users/:id', UserController.update)
@@ -9,5 +10,9 @@ router.get('/users', UserController.index)
 router.get('/users/:id', UserController.show)
 router.delete('/users', UserController.wipe)
 router.delete('/users/:id', UserController.delete)
+
+router.post('/session', SessionController.create)
+router.get('/session', SessionController.index)
+router.get('/session/:id', SessionController.show)
 
 module.exports = router
